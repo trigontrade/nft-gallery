b20099100b1b392d672089d046e7f77df9c369e2
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-01-17 15:22:48 +0000

    Update parameters.ts

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -1,7 +1,7 @@
 import CustomChain from "../consts/chain";
 
-export const contractConst = "0x5Baa53AfB5b14aA3e7c70D649D56cfa00BA324eE";
+export const contractAddress = "0x5Baa53AfB5b14aA3e7c70D649D56cfa00BA324eE";
 
-export const chainConst = CustomChain;
+export const chain = CustomChain;
 
 export const blockExplorer = "https://scan.bit-rock.io";
